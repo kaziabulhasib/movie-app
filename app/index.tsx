8cb3319072fa7f9d3cb2cdc09cc18c5b6a47e13d
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-24 23:01:51 +0530

    file setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function Index() {
+  return (
+    <View className=' flex-1 justify-center items-center '>
+      <Text className=' text-red-400 text-3xl font-bold'>Welcome to my app</Text>
+    </View>
+  );
+}
