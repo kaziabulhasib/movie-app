eec53c969c874ae9d36ea8fbc3b4d1d9d14ce101
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-24 23:48:05 +0530

    style setup : final

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,9 @@ import "./global.css";
 export default function Index() {
   return (
     <View className=' flex-1 justify-center items-center '>
-      <Text className=' text-red-400 text-3xl font-bold'>Welcome to my app</Text>
+      <Text className='  text-3xl font-bold text-dark-200'>
+        Welcome to my app
+      </Text>
     </View>
   );
 }
