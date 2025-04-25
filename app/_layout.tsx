51ddef500ecc2f4ac580d2e622eb1c1634068bfd
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-25 10:48:12 +0530

    navigation tabs set

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,7 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name='(tabs)'
-        options={{ headerShown: false }}></Stack.Screen>
+      
       <Stack.Screen
         name='movies/id'
         options={{ headerShown: false }}></Stack.Screen>
