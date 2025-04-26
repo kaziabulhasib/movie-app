13aab94b7140045c6ca3921fc0f08eb1346f9789
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-26 23:41:42 +0530

    dynamic tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Text, View } from "react-native";
-// import "./global.css";
-// import "../../app/global.css";
+
 import { Link } from "expo-router";
 
 export default function Index() {
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <Stack>
-      
+      <Stack.Screen
+        name='(tabs)'
+        options={{ headerShown: false }}></Stack.Screen>
       <Stack.Screen
         name='movies/id'
         options={{ headerShown: false }}></Stack.Screen>
