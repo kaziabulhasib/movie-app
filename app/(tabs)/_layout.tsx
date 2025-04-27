d6a0a9ca70795a72c76bbeb0a80697d434858279
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-27 22:40:00 +0530

    tab bar  set final

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,31 @@
-import { View, Text, ImageBackground, Image } from "react-native";
-import React from "react";
 import { Tabs } from "expo-router";
-import { images } from "@/constants/images";
-import { icons } from "@/constants/icons";
+import { ImageBackground, Image, Text, View } from "react-native";
 
-const TabIcon = ({ focused, icon, title }: any) => {
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+function TabIcon({ focused, icon, title }: any) {
   if (focused) {
     return (
       <ImageBackground
         source={images.highlight}
-        className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden '>
+        className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'>
         <Image source={icon} tintColor='#151312' className='size-5' />
-        <Text className='text-secondary text-base font-semibold'>{title}</Text>
+        <Text className='text-secondary text-base font-semibold ml-2'>
+          {title}
+        </Text>
       </ImageBackground>
     );
   }
+
   return (
-    <View className='size-fit justify-center items-center mt-4 rounded-full'>
-      <Image source={icon} tintColor='#A8B5DB' />
+    <View className='size-full justify-center items-center mt-4 rounded-full'>
+      <Image source={icon} tintColor='#A8B5DB' className='size-5' />
     </View>
   );
-};
-const _layout = () => {
+}
+
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
@@ -33,7 +37,7 @@ const _layout = () => {
           alignItems: "center",
         },
         tabBarStyle: {
-          backgroundColor: "#0f0d23",
+          backgroundColor: "#0F0D23",
           borderRadius: 50,
           marginHorizontal: 20,
           marginBottom: 36,
@@ -41,59 +45,52 @@ const _layout = () => {
           position: "absolute",
           overflow: "hidden",
           borderWidth: 1,
-          borderColor: "#0f0d23",
+          borderColor: "#0F0D23",
         },
       }}>
       <Tabs.Screen
         name='index'
         options={{
-          title: "Home",
+          title: "index",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} icon={icons.home} title='Home' />
-            </>
+            <TabIcon focused={focused} icon={icons.home} title='Home' />
           ),
         }}
       />
-      <Tabs.Screen
-        name='saved'
-        options={{
-          title: "Saved",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} icon={icons.save} title='Saved' />
-            </>
-          ),
-        }}
-      />
+
       <Tabs.Screen
         name='search'
         options={{
           title: "Search",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} icon={icons.search} title='Search' />
-            </>
+            <TabIcon focused={focused} icon={icons.search} title='Search' />
           ),
         }}
       />
+
+      <Tabs.Screen
+        name='save'
+        options={{
+          title: "Save",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title='Save' />
+          ),
+        }}
+      />
+
       <Tabs.Screen
         name='profile'
         options={{
           title: "Profile",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} icon={icons.person} title='Profile' />
-            </>
+            <TabIcon focused={focused} icon={icons.person} title='Profile' />
           ),
         }}
       />
     </Tabs>
   );
-};
-
-export default _layout;
+}
