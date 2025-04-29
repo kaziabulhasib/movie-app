1888b7bed68d3c226e0c851b6220759ffbfc9cf4
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-29 11:29:58 +0530

    fetch movie with search

--- services/api.ts
+++ services/api.ts
@@ -8,7 +8,12 @@ export const TMDB_CONFIG={
 }
 
 export const fetchPopularMovies = async ({ query }: { query: string }) => {
-    const endpoint = `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    const endpoint = 
+    query ? 
+    `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
+    : 
+    
+    `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: "GET",
